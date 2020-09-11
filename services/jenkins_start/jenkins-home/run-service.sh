8296afc45c7ea4af25a61484c73416e557147520
Vrx261 <Vrx261@gmail.com>
2020-09-11 16:13:11 +0300

    path fix

--- services/jenkins_start/jenkins-home/run-service.sh
+++ services/jenkins_start/jenkins-home/run-service.sh
@@ -14,7 +14,7 @@ tag=$1
 envFileContent=""
 if [ "$environmentRepo" == "Custom" ]
 then
-envFileContent=$(cat /var/jenkins_home/workspace/test/environment/service-env)
+envFileContent=$(cat /var/jenkins_home/workspace/${serviceName}/environment/service-env)
 else
 echo "Environment repository: $environmentRepo"
 envFileContent=$(curl -H "Authorization: token ${githubToken}" ${environmentRepo}/${serviceName}/${serviceName} 2> /dev/null)
