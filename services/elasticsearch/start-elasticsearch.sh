7a156f298b06267c606d35ac1f0e5cbeb04ac044
Vrx261 <Vrx261@gmail.com>
2020-09-15 15:56:39 +0300

    removed ssh volumes for one file configs

--- services/elasticsearch/start-elasticsearch.sh
+++ services/elasticsearch/start-elasticsearch.sh
@@ -4,13 +4,12 @@ cd "$(dirname "$0")"
 
 dockerManagerIp=${DOCKER_MANAGER_IP}
 
-mkdir -p /etc/docker/shared/elasticsearch/cfg
-cp elasticsearch.yml /etc/docker/shared/elasticsearch/cfg/elasticsearch.yml
 mkdir -p /etc/docker/shared/elasticsearch/data
 
+docker config create elasticsearch elasticsearch.yml
+
 docker service create --name elasticsearch --network default_network --replicas 1 \
---mount volume-driver=vieux/sshfs,source=elasticsearch-cfg,target=/usr/share/elasticsearch/config/elasticsearch.yml,volume-opt=sshcmd=root@${dockerManagerIp}:/etc/docker/shared/elasticsearch/cfg/elasticsearch.yml,volume-opt=allow_other,volume-opt=password=${ROOT_PASSWORD} \
+--config src=elasticsearch,target=/usr/share/elasticsearch/config/elasticsearch.yml \
 --mount volume-driver=vieux/sshfs,source=elasticsearch-data,target=/usr/share/elasticsearch/data,volume-opt=sshcmd=root@${dockerManagerIp}:/etc/docker/shared/elasticsearch/data,volume-opt=allow_other,volume-opt=password=${ROOT_PASSWORD} \
 --env ES_JAVA_OPTS='-Xmx256m -Xms256m' --env ELASTIC_PASSWORD=elasticP --env discovery.type=single-node docker.elastic.co/elasticsearch/elasticsearch:7.8.0
-
 
--- services/kibana/start-kibana.sh
+++ services/kibana/start-kibana.sh
@@ -2,9 +2,8 @@
 
 cd "$(dirname "$0")"
 
-dockerManagerIp=${DOCKER_MANAGER_IP}
+docker config create kibana kibana.yml
 
-mkdir -p /etc/docker/shared/kibana
-cp kibana.yml /etc/docker/shared/kibana.yml
-
-docker service create --name kibana --network default_network --replicas 1 -p 5601:5601 --mount volume-driver=vieux/sshfs,source=kibana,target=/usr/share/kibana/config,volume-opt=sshcmd=root@${dockerManagerIp}:/etc/docker/shared/kibana,volume-opt=allow_other,volume-opt=password=${ROOT_PASSWORD} docker.elastic.co/kibana/kibana:7.8.0
+docker service create --name kibana --network default_network --replicas 1 -p 5601:5601 \
+--config src=kibana,target=/usr/share/kibana/config/kibana.yml \
+docker.elastic.co/kibana/kibana:7.8.0
