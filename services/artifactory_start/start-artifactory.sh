9ab2196e48e775f3d081456e7fc2153398476218
root <root@localhost.localdomain>
2020-09-28 17:24:26 -0400

    Add default artifactory system settings

--- services/artifactory_start/start-artifactory.sh
+++ services/artifactory_start/start-artifactory.sh
@@ -2,8 +2,11 @@
 
 mkdir -p /mounts/artifactory
 chown -R 1030 /mounts/
-
+yum install curl
+cp -r ./base /mounts/artifactory/exp/
 firewall-cmd --zone=trusted --add-port=8081/tcp --permanent
 firewall-cmd --zone=trusted --add-port=8082/tcp --permanent
 
 docker run -d --name artifactory --network=j-a-net --restart=always -v /mounts/artifactory:/var/opt/jfrog/artifactory -p 8081:8081 -p 8082:8082 -e EXTRA_JAVA_OPTIONS='-Xms512m -Xmx1024m -Xss256k -XX:+UseG1GC' docker.bintray.io/jfrog/artifactory-oss:7.6.2
+sleep 300
+curl -X POST -u admin:password -H "Content-type: application/json" -d '{ "importPath" : "/var/opt/jfrog/artifactory/exp/base" }'  http://localhost:8081/artifactory/api/import/system
