0e390c790ef07e88b600de5980c99b853b5f0adb
root <root@localhost.localdomain>
2020-10-02 08:42:24 -0400

    Not using sleep for importing

--- services/artifactory_start/start-artifactory.sh
+++ services/artifactory_start/start-artifactory.sh
@@ -3,11 +3,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo ${DIR}
 mkdir -p /mounts/artifactory
 chown -R 1030 /mounts/
-yum install curl
+yum install curl at
+systemctl start atd
 cp -r ${DIR}/base /mounts/artifactory/exp/
 firewall-cmd --zone=trusted --add-port=8081/tcp --permanent
 firewall-cmd --zone=trusted --add-port=8082/tcp --permanent
 
 docker run -d --name artifactory --network=j-a-net --restart=always -v /mounts/artifactory:/var/opt/jfrog/artifactory -p 8081:8081 -p 8082:8082 -e EXTRA_JAVA_OPTIONS='-Xms512m -Xmx1024m -Xss256k -XX:+UseG1GC' docker.bintray.io/jfrog/artifactory-oss:7.6.2
-sleep 300
-curl -X POST -u admin:password -H "Content-type: application/json" -d '{ "importPath" : "/var/opt/jfrog/artifactory/exp/base" }'  http://localhost:8081/artifactory/api/import/system
+echo "${DIR}/importSystem.sh" | at now +5 min
