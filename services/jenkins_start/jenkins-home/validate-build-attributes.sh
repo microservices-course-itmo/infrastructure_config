dbe140415c02725fa2bd6fc413d185e73942cb1b
Артем Крылов <kav128@outlook.com>
2020-10-13 17:04:00 +0300

    add distributionManagement and dockerfile-maven-plugin validation

--- services/jenkins_start/jenkins-home/validate-build-attributes.sh
+++ services/jenkins_start/jenkins-home/validate-build-attributes.sh
@@ -11,7 +11,23 @@ currentVersion_FORMAT="^([0-9]+\.)+[0-9]+$"
 [[ ${scmBranch} =~ ${scmBranch_FORMAT} ]] || (echo "Bad branch $scmBranch" && exit 1)
 [[ ${currentVersion} =~ ${currentVersion_FORMAT} ]] || (echo "Bad version $currentVersion" && exit 2)
 
+[[ $(xpath -q -e '//project/distributionManagement/' $filename | grep -o '<distributionManagement>' | wc -l) -lt "1" ]] \
+    && echo "You must specify distributionManagement section in your pom" \
+    && echo "<distributionManagement>
+    <snapshotRepository>
+        <id>snapshots</id>
+        <url>http://artifactory:8081/artifactory/libs-snapshot-local</url>
+    </snapshotRepository>
+</distributionManagement>" \
+    && exit 3
 
+artifact_id="$(xpath -q -e '//project/artifactId/text()' $filename)"
+service_FORMAT="-service$"
 
+if [[ ${artifact_id} =~ ${service_FORMAT} ]]; then
+    [[ $(xpath -q -e '//project/build/plugins/plugin/artifactId/text()' $filename | grep "dockerfile-maven-plugin" | wc -l) -lt "1" ]] \
+    && echo "You must specify dockerfile-maven-plugin in your pom" \
+    && exit 4
+fi
 
 # sudo apt install libxml-xpath-perl -- отсюда команда xpath
