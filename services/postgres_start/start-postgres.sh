14694a82096b9fc0ebafc8890cd0026acdf7b56b
Romaniuk Vladimir <romaniukvr@yandex.ru>
2020-10-18 17:29:16 -0400

    New line at crontab

--- services/postgres_start/start-postgres.sh
+++ services/postgres_start/start-postgres.sh
@@ -16,7 +16,7 @@ ssh $(docker service ps postgres | sed -n 2p | awk '{print $4}')
 
 docker exec -it $( docker ps -f name=postgres -q ) bash -c 'apt update; apt install cron -y'
 
-docker exec -it $( docker ps -f name=postgres -q ) bash -c '(echo -e "0 0 * * * /var/lib/postgresql/data/scripts/create-backup.sh\n"; echo "*/5 * * * * /var/lib/postgresql/data/scripts/update-connections-amount.sh") | crontab - '
+docker exec -it $( docker ps -f name=postgres -q ) bash -c '(echo -e "0 0 * * * /var/lib/postgresql/data/scripts/create-backup.sh\n"; echo -e "*/5 * * * * /var/lib/postgresql/data/scripts/update-connections-amount.sh\n") | crontab - '
 
 docker exec -it $( docker ps -f name=postgres -q ) bash -c 'cron'
 
