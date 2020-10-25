3892bd19ef629362fcbfc4333ac82533df447e8f
Vladimir Romaniuk <romaniukvr@yandex.ru>
2020-10-25 19:13:25 -0400

    Add env variable SLACK_WEBHOOK

--- services/slack_cronjobs_start/scripts/daily-digest.sh
+++ services/slack_cronjobs_start/scripts/daily-digest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-webhook="https://hooks.slack.com/services/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"
+webhook="${SLACK_WEBHOOK}"
 header="Состояние на "$( date +%d/%m/%Y )
 services="Сервисы:\n"$(docker service ls | grep "5000" | awk '{ split($4,amount,"/"); split($5,image,"/"); if ( amount[1] == "0" ) status="не " ; print $2, status,"запущен с образом ", image[2] }')
 memory="Память:\n"$( df -h | grep "/dev/sd" | awk '{ print $4, "/", $2, " свободно в ", $6 }')
