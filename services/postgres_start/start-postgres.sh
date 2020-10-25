d5f3d74a79ca5febd8c259406c55d80aea6d622a
Vladimir Romaniuk <romaniukvr@yandex.ru>
2020-10-25 11:54:56 -0400

    Changed exec to dockerfile

--- services/postgres_start/start-postgres.sh
+++ services/postgres_start/start-postgres.sh
@@ -8,16 +8,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 chmod +x $DIR/scripts/*
 
-cp -r "$DIR/scripts" "/etc/docker/shared/postgres"
+docker build -t postgres_with_backup $DIR
 
-docker service create --name postgres --network default_network --replicas 1 --mount volume-driver=vieux/sshfs,source=postgres,target=/var/lib/postgresql/data,volume-opt=sshcmd=root@${dockerManagerIp}:/etc/docker/shared/postgres,volume-opt=allow_other,volume-opt=password=${ROOT_PASSWORD} --env POSTGRES_PASSWORD=postgres postgres
-
-ssh $(docker service ps postgres | sed -n 2p | awk '{print $4}')
-
-docker exec -it $( docker ps -f name=postgres -q ) bash -c 'apt update; apt install cron -y'
-
-docker exec -it $( docker ps -f name=postgres -q ) bash -c '(echo -e "0 0 * * * /var/lib/postgresql/data/scripts/create-backup.sh\n"; echo -e "*/5 * * * * /var/lib/postgresql/data/scripts/update-connections-amount.sh\n") | crontab - '
-
-docker exec -it $( docker ps -f name=postgres -q ) bash -c 'cron'
-
-exit
+docker service create --name postgres --network default_network --replicas 1 --mount volume-driver=vieux/sshfs,source=postgres,target=/var/lib/postgresql/data,volume-opt=sshcmd=root@${dockerManagerIp}:/etc/docker/shared/postgres,volume-opt=allow_other,volume-opt=password=${ROOT_PASSWORD} --env POSTGRES_PASSWORD=postgres postgres_with_backup
