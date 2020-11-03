3e61e349d955ee65fdaf12d38ce337cf934a42e6
Daniel Kunis <dankunis@protonmail.com>
2020-11-03 13:52:09 +0300

    Added CASC plugin + project-based matrix conf + set initial pass

--- services/jenkins_start/start-jenkins.sh
+++ services/jenkins_start/start-jenkins.sh
@@ -22,14 +22,10 @@ then
 docker build -t jenkins .
 fi
 
-initPassword=$(docker run --name jenkins --network=j-a-net --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /mounts/jenkins/home:/home -e DOCKER_REPO_HOST=${dockerRepoHost} -e GITHUB_TOKEN=${githubToken} -p 8090:8090 jenkins 2>&1 | grep -m1 -A2 "Please use the following password to proceed to installation:" | (tail -n 1 && docker stop jenkins > /dev/null))
-echo "Password: $initPassword"
-
 docker start jenkins > /dev/null
 sleep 60
 wget --continue --content-on-error --retry-connrefused --tries=10 --waitretry=120 http://${jenkinsIp}:8090/jnlpJars/jenkins-cli.jar
 cp jenkins-cli.jar jenkins-home/jenkins-cli.jar
-echo 'jenkins.model.Jenkins.instance.securityRealm.createAccount("admin", '\"${adminPass}\"')' | java -jar jenkins-cli.jar -s http://${jenkinsIp}:8090 -auth admin:${initPassword} -noKeyAuth groovy = –
 
 sleep 10
 
