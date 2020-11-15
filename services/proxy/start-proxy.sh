da1a127eb48e38d91c9ad756ba8e4179dcae3db8
Eduard Glekler <eduard.glekler@yandex.ru>
2020-11-15 17:13:30 +0300

    proxy fix

--- services/proxy/start-proxy.sh
+++ services/proxy/start-proxy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+name=$1
+
 firewall-cmd --zone=trusted --change-interface=docker_gwbridge
 firewall-cmd --permanent --zone=trusted --add-port=2375/tcp
 firewall-cmd --reload
 
-ip = $(ip addr | grep eth0 | tail -n 1 | awk '{print $2}' | awk -F '/' '{print $1}')
-docker run --name container_proxy --user root -v ${PWD}/main.conf:/etc/nginx/nginx.conf --add-host host.docker.internal:$ip --network test_network -d nginx;
+ip=$(ip addr | grep eth0 | tail -n 1 | awk '{print $2}' | awk -F '/' '{print $1}')
+echo $ip
+docker run --name $name --user root -v ${PWD}/main.conf:/etc/nginx/nginx.conf --add-host host.docker.internal:$ip --network test_network -d nginx;
