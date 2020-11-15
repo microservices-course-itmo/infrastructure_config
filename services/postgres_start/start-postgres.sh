e82b26bef788939ef1c025d910e8d3ce3bb64068
Romaniuk Vladimir <32132706+ImpurestPath@users.noreply.github.com>
2020-11-16 00:59:32 +0300

    Update start-postgres.sh

--- services/postgres_start/start-postgres.sh
+++ services/postgres_start/start-postgres.sh
@@ -10,4 +10,6 @@ chmod +x $DIR/scripts/*
 
 docker build -t 0.0.0.0:25001/postgres_with_backup $DIR
 
+docker push 0.0.0.0:25001/postgres_with_backup
+
 docker service create --name postgres --network default_network --replicas 1 --mount volume-driver=vieux/sshfs,source=postgres,target=/var/lib/postgresql/data,volume-opt=sshcmd=root@${dockerManagerIp}:/etc/docker/shared/postgres,volume-opt=allow_other,volume-opt=password=${ROOT_PASSWORD} --env POSTGRES_PASSWORD=postgres  0.0.0.0:25001/postgres_with_backup
