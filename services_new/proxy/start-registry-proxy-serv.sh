03dbe9f77a9ce34e0883b01e4b5066d590768ae2
egoarka <egoarka@gmail.com>
2021-03-10 14:57:06 +0300

    feat: add registry proxy in proxy stack

--- services_new/proxy/start-registry-proxy-serv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker config create registry-proxy-serv registry-proxy-serv.conf
-docker service create --name registry-proxy-serv --user root --config src=registry-proxy-serv,target=/etc/nginx/nginx.conf --network default_network nginx
--- services_new/proxy/start-registry-proxy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --name registry-proxy --user root -v ${PWD}/registry-proxy.conf:/etc/nginx/nginx.conf --network default_network -p 25001:25001 -d nginx;
