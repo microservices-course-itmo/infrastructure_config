52717de8eaf1290abb82e888740bd1af7074632e
Vrx261 <Vrx261@gmail.com>
2021-03-10 20:18:47 +0300

    mongo fix, memory limit, jenkins frontend scripts

--- services/mongo_start/start-mongo.sh
+++ services/mongo_start/start-mongo.sh
@@ -5,5 +5,5 @@ dockerManagerIp=${DOCKER_MANAGER_IP}
 mkdir -p /etc/docker/shared/mongo
 
 docker service create --replicas 1 --network default_network \
---mount volume-driver=vieux/sshfs,source=mongodata,target=/data,volume-opt=sshcmd=root@${dockerManagerIp}:/etc/docker/shared/mongo,volume-opt=allow_other,volume-opt=password=${ROOT_PASSWORD} \
+--mount type=bind,source=/etc/docker/shared/mongo/db,destination=/data/db \
 -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASWORD=${MONGO_PASSWORD} --name mongo -p 27017:27017 mongo
