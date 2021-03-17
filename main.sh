e343fb72081b436a00e822cc2b8eb55b5fb3a348
egoarka <egoarka@gmail.com>
2021-03-17 18:58:58 +0300

    feat: specify chown feat: specify users chore: wip elk .env

--- main.sh
+++ main.sh
@@ -14,20 +14,34 @@ test -e ./.env || { echo ".env not found" ; exit 1; }
 
 export $(xargs <.env)
 
-./services/docker_start/start-*.sh
+mkdir -p /etc/docker/shared/registry
+mkdir -p /etc/docker/shared/artifactory
+mkdir -p /etc/docker/shared/zookeeper
+mkdir -p /etc/docker/shared/prometheus
+mkdir -p /etc/docker/shared/grafana
+mkdir -p /etc/docker/shared/mongo/db
+mkdir -p /etc/docker/shared/elasticsearch
+
+# chown -R 1001:1001 /etc/docker/shared/zookeeper
+# chown -R 1000:1000 /etc/docker/shared/prometheus
+# chown -R 472:1 /etc/docker/shared/grafana
+# chown -R 1000:1000 /etc/docker/shared/elasticsearch
+
+# ./services/docker_start/start-*.sh
 # ./services/gluster_start/start-*.sh
 
-docker stack deploy -c ./services_new/proxy/docker-compose.yml proxies
-
 docker stack deploy -c ./services_new/registry/docker-compose.yml registry
-docker stack deploy -c ./services_new/artifactory/docker-compose.yml artifactory
+
+docker stack deploy --with-registry-auth -c ./services_new/proxy/docker-compose.yml proxies
+
+docker stack deploy --with-registry-auth -c ./services_new/artifactory/docker-compose.yml artifactory
 
 # ./services/jenkins_start/start-*.sh
 
-docker stack deploy -c ./services_new/zoopark/docker-compose.yml zoopark
-docker stack deploy -c ./services_new/monitoring/docker-compose.yml monitoring
+docker stack deploy --with-registry-auth -c ./services_new/zoopark/docker-compose.yml zoopark
+docker stack deploy --with-registry-auth -c ./services_new/monitoring/docker-compose.yml monitoring
 
 # ./services/postgres_start/start-*.sh
 
-docker stack deploy -c ./services_new/mongo/docker-compose.yml mongo
-docker stack deploy -c ./services_new/elk/docker-compose.yml elk
+docker stack deploy --with-registry-auth -c ./services_new/mongo/docker-compose.yml mongo
+docker stack deploy --with-registry-auth -c ./services_new/elk/docker-compose.yml elk 
