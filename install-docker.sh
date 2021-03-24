57d55cf77a469344810d8bcf142a236ab824cdd1
egoarka <egoarka@gmail.com>
2021-03-24 19:01:26 +0300

    chore: add install docker script

---
+++ install-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+yum check-update
+
+curl -fsSL https://get.docker.com/ | sh
+
+curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
