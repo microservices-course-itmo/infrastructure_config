921ba19d894470efbb065d41466c8817adae38a7
egoarka <egoarka@gmail.com>
2021-03-24 20:14:41 +0300

    chore: get rid of docker gw bridge

--- firewall.sh
+++ firewall.sh
@@ -13,10 +13,6 @@ firewall-cmd --add-port=4789/udp --permanent
 firewall-cmd --zone=trusted --add-port=8081/tcp --permanent
 firewall-cmd --zone=trusted --add-port=8082/tcp --permanent
 
-# docker daemon ports
-firewall-cmd --zone=trusted --change-interface=docker_gwbridge
-firewall-cmd --permanent --zone=trusted --add-port=2375/tcp
-
 # kibana ports
 firewall-cmd --zone=trusted --add-port=5601/tcp --permanent
 
